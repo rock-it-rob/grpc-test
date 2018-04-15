ac2b235d9121efd064359ebaefdfd289c012b399
Rob Benton <rob.benton7@gmail.com>
2018-04-14 19:13:17 -0500

    Added a starter but we should use spring boot instead.

---
+++ server/src/main/java/rob/proto/grpc/Start.java
@@ -0,0 +1,23 @@
+package rob.proto.grpc;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import rob.proto.grpc.server.GrpcServer;
+
+import java.io.IOException;
+
+/**
+ * Start is the main entry point for the application.
+ *
+ * @author rob
+ */
+public class Start
+{
+    private static final String SPRING_XML = "classpath:server-spring.xml";
+
+    public static void main(String[] args) throws IOException
+    {
+        GenericXmlApplicationContext applicationContext = new GenericXmlApplicationContext(SPRING_XML);
+        GrpcServer grpcServer = applicationContext.getBean(GrpcServer.class);
+        grpcServer.start();
+    }
+}
